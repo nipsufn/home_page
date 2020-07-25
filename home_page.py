cb5eb1df26dc55375dd29f22596e1139dcd4a794
nipsufn <ferdynand.naczynski@gmail.com>
2020-07-25 08:48:59 +0200

    fix volume

--- home_page.py
+++ home_page.py
@@ -8,7 +8,7 @@ def index():
     APP.logger.error("%s", str(request.form))
     if 'xmms2url' in request.form:
         if 'xmms2request' in request.form and request.form['xmms2request'] == 'on':
-            subprocess.Popen('/usr/bin/xmms2 server volume 100', shell=True)
+            subprocess.Popen('/usr/bin/xmms2 server volume ' + request.form['xmms2volume'], shell=True)
             subprocess.Popen('/usr/bin/xmms2 add ' + request.form['xmms2url'], shell=True)
             subprocess.Popen('/usr/bin/xmms2 play', shell=True)
         else:
@@ -19,7 +19,7 @@ def index():
     xmms2playing.communicate()[0]
     xmms2playing = xmms2playing.returncode
 
-    xmms2volume = subprocess.Popen('xmms2 server volume | head -n1 | cut -d' ' -f3', shell=True)
+    xmms2volume = subprocess.Popen('xmms2 server volume | head -n1 | cut -d\' \' -f3', shell=True)
     xmms2volume = xmms2volume.communicate()[0]
 
     APP.logger.error("%s", str(xmms2playing))
