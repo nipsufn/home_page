bc702d2f9070426a0397c83504ff62c34da8a9e1
nipsufn <ferdynand.naczynski@gmail.com>
2020-07-25 09:07:27 +0200

    fix volume

--- home_page.py
+++ home_page.py
@@ -20,7 +20,7 @@ def index():
     xmms2playing = xmms2playing.returncode
 
     xmms2volume = subprocess.Popen('/usr/bin/xmms2 server volume | head -n1 | cut -d\' \' -f3', shell=True)
-    xmms2volume = xmms2volume.communicate()[0]
+    xmms2volume2 = xmms2volume.communicate()[0]
 
-    APP.logger.error("%s", str(xmms2playing))
-    return render_template("index.html.j2", xmms2playing=xmms2playing, xmms2volume=xmms2volume)
+    APP.logger.error("%s", str(xmms2volume2))
+    return render_template("index.html.j2", xmms2playing=xmms2playing, xmms2volume=xmms2volume2)
